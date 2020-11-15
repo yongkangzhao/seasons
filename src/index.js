a7cb456880d61b40cf034f37a278d3c55bc0f098
Yongkang Zhao <cookieyong@gmail.com>
2020-11-14 21:59:47 -0800

    moved initial setstate codes into componentDidMount method for better coding style practice.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ class App extends React.Component {
         super(props);
         this.state = { lat: null, errorMessage: ''};
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position)=> {
-                this.setState({ lat: position.coords.latitude })
-            },
-            (err) => {
-                this.setState({errorMessage: err.message});
-            }
-        );
     }
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> { this.setState({ lat: position.coords.latitude })},
+            (err) => { this.setState({errorMessage: err.message})}
+        );
+    };
+
     render() {
         if (this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>;
