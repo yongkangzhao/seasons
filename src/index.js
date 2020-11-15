28a40b3d4538c3315c51964b3b7b13111ac2865d
Yongkang Zhao <cookieyong@gmail.com>
2020-11-14 21:57:20 -0800

    updated render to display different data based on different states

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-    render() {
+    constructor(props){
+        super(props);
+        this.state = { lat: null, errorMessage: ''};
+
         window.navigator.geolocation.getCurrentPosition(
-            (position)=> console.log(position),
-            (err) => console.log(err)
+            (position)=> {
+                this.setState({ lat: position.coords.latitude })
+            },
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
         );
-        return <div>Latitude: </div>;
+    }
+    render() {
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+        return <div>Loading...</div>;
+        
     }
 }
 
