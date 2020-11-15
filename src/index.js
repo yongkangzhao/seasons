cf2097bd2089671db783f840f3aac1fce81ef770
Yongkang Zhao <cookieyong@gmail.com>
2020-11-14 23:00:54 -0800

    added a helper function to help clean up the code style

--- src/index.js
+++ src/index.js
@@ -17,15 +17,22 @@ class App extends React.Component {
         );
     };
 
-    render() {
+    renderContent(){
         if (this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat){
             return <SeasonDisplay lat={this.state.lat}/>;
         }
-        return <Spinner message="Please accept location request..."/>;
-        
+            return <Spinner message="Please accept location request..."/>;
+    }
+
+    render() {
+        return (
+            <div>
+            {this.renderContent()}
+            </div>    
+        );
     }
 }
 
